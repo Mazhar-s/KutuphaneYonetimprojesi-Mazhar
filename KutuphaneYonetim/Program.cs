7ec73c5b80f82828f1ebf580eb74f5c2ad1119bd
Mazhar <mazhar@deneme.com>
2025-12-23 20:07:10 +0300

    Not defteri ile UTF8 yapildi

--- KutuphaneYonetim/Program.cs
+++ KutuphaneYonetim/Program.cs
@@ -5,8 +5,10 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+// Veritabaný adýný V3 yaptýk ki tertemiz Türkçe kurulsun
 builder.Services.AddDbContext<KutuphaneYonetimContext>(options =>
-    options.UseSqlite("Data Source=Kutuphane.db"));
+    options.UseSqlite("Data Source=KutuphaneV3.db"));
 
 var app = builder.Build();
 
@@ -17,7 +19,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles(); // Resimlerin çalýþmasý için þart
+app.UseStaticFiles();
 
 app.UseRouting();
 app.UseAuthorization();
@@ -26,72 +28,65 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-// VERÝTABANI TEMÝZLÝK VE KURULUM
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<KutuphaneYonetimContext>();
 
-    // Veritabanýný güncelle
     context.Database.Migrate();
 
-    // 1. ESKÝ BOZUK KAYITLARI SÝL (Soru iþaretli olanlar gitsin)
-    var eskiKayitlar = context.Kitaplar.ToList();
-    context.Kitaplar.RemoveRange(eskiKayitlar);
-    context.SaveChanges();
-
-    // 2. TEMÝZ VERÝLERÝ EKLE
-    // Not: Ýsimleri Ýngilizce karakterle (c, s, u) yazýyoruz. 
-    // Böylece sunucu "Bu ne?" demez, yazýlar düzgün çýkar.
-    // Site açýlýnca "Düzenle" diyip Türkçe yapabilirsin.
-    context.Kitaplar.AddRange(
-        new Kitap
-        {
-            Ad = "Ucurtma Avcisi",
-            Yazar = "Khaled Hosseini",
-            SayfaSayisi = 375,
-            Tur = "Roman",
-            BasimYili = 2003,
-            ResimYolu = "/images/ucurtma.jpg"
-        },
-        new Kitap
-        {
-            Ad = "OD",
-            Yazar = "Iskender Pala",
-            SayfaSayisi = 358,
-            Tur = "Roman",
-            BasimYili = 2011,
-            ResimYolu = "/images/od.jpg"
-        },
-        new Kitap
-        {
-            Ad = "Kafkas Kartali",
-            Yazar = "Yavuz Bahadiroglu",
-            SayfaSayisi = 216,
-            Tur = "Roman",
-            BasimYili = 1990,
-            ResimYolu = "/images/seyhsamil.jpg"
-        },
-        new Kitap
-        {
-            Ad = "Cile",
-            Yazar = "Necip Fazil Kisakurek",
-            SayfaSayisi = 502,
-            Tur = "Siir",
-            BasimYili = 1962,
-            ResimYolu = "/images/cile.png"
-        },
-        new Kitap
-        {
-            Ad = "Kurk Mantolu Madonna",
-            Yazar = "Sabahattin Ali",
-            SayfaSayisi = 160,
-            Tur = "Roman",
-            BasimYili = 1943,
-            ResimYolu = "/images/madonna.jpg"
-        }
-    );
-    context.SaveChanges();
+    // Veritabaný boþsa TÜRKÇE verileri ekle
+    if (!context.Kitaplar.Any())
+    {
+        context.Kitaplar.AddRange(
+            new Kitap
+            {
+                Ad = "Uçurtma Avcýsý",
+                Yazar = "Khaled Hosseini",
+                SayfaSayisi = 375,
+                Tur = "Roman",
+                BasimYili = 2003,
+                ResimYolu = "/images/ucurtma.jpg"
+            },
+            new Kitap
+            {
+                Ad = "OD",
+                Yazar = "Ýskender Pala",
+                SayfaSayisi = 358,
+                Tur = "Roman",
+                BasimYili = 2011,
+                ResimYolu = "/images/od.jpg"
+            },
+            new Kitap
+            {
+                Ad = "Kafkas Kartalý – Þeyh Þamil",
+                Yazar = "Yavuz Bahadýroðlu",
+                SayfaSayisi = 216,
+                Tur = "Roman",
+                BasimYili = 1990,
+                ResimYolu = "/images/seyhsamil.jpg"
+            },
+            new Kitap
+            {
+                Ad = "Çile",
+                Yazar = "Necip Fazýl Kýsakürek",
+                SayfaSayisi = 502,
+                Tur = "Þiir",
+                BasimYili = 1962,
+                ResimYolu = "/images/cile.png"
+            },
+            new Kitap
+            {
+                Ad = "Kürk Mantolu Madonna",
+                Yazar = "Sabahattin Ali",
+                SayfaSayisi = 160,
+                Tur = "Roman",
+                BasimYili = 1943,
+                ResimYolu = "/images/madonna.jpg"
+            }
+        );
+        context.SaveChanges();
+    }
 }
 
 app.Run();
