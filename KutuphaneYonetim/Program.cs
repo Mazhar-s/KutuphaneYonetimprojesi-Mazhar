c16e9a8d9205e72d1b3ad175b6fdbf10cfc1451a
Mazhar <mazhar@deneme.com>
2025-12-23 14:59:47 +0300

    Auto migration eklendi

--- KutuphaneYonetim/Program.cs
+++ KutuphaneYonetim/Program.cs
@@ -34,4 +34,13 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<KutuphaneYonetim.Data.KutuphaneContext>();
+    context.Database.Migrate();
+}
+
+app.Run();
+
 app.Run();
