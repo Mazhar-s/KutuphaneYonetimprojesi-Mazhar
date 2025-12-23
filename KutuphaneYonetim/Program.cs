13f80087834c992b8264b57d6a28880500b76b0c
Mazhar <mazhar@deneme.com>
2025-12-23 17:54:55 +0300

    Final: Seed data, Turkce mesajlar ve model uyumu tamamlandi

--- KutuphaneYonetim/Program.cs
+++ KutuphaneYonetim/Program.cs
@@ -1,5 +1,7 @@
-using Microsoft.EntityFrameworkCore;
 using KutuphaneYonetim.Data;
+using KutuphaneYonetim.Models;
+using Microsoft.EntityFrameworkCore;
+using KutuphaneYonetim.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -39,6 +41,57 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<KutuphaneYonetim.Data.KutuphaneYonetimContext>();
     context.Database.Migrate();
+    if (!context.Kitaplar.Any())
+    {
+        context.Kitaplar.AddRange(
+            new Kitap
+            {
+                Ad = "Uçurtma Avcýsý", // KitapAdý deðil, sende "Ad" yazýyor
+                Yazar = "Khaled Hosseini",
+                SayfaSayisi = 375, // SayfaSayýsý deðil, sende "SayfaSayisi" (ý harfi yok)
+                Tur = "Roman",
+                BasimYili = 2003, // Bu alan sende zorunlu, eklemezsek hata verir
+                ResimYolu = "https://img.kitapyurdu.com/v1/getImage/fn:11333160/wh:true/wi:220" // KapakResmi deðil, "ResimYolu"
+            },
+            new Kitap
+            {
+                Ad = "OD",
+                Yazar = "Ýskender Pala",
+                SayfaSayisi = 358,
+                Tur = "Roman",
+                BasimYili = 2011,
+                ResimYolu = "https://img.kitapyurdu.com/v1/getImage/fn:11267812/wh:true/wi:220"
+            },
+            new Kitap
+            {
+                Ad = "Kafkas Kartalý – Þeyh Þamil",
+                Yazar = "Yavuz Bahadýroðlu",
+                SayfaSayisi = 216,
+                Tur = "Biyografik Roman",
+                BasimYili = 1990,
+                ResimYolu = "https://img.kitapyurdu.com/v1/getImage/fn:11252192/wh:true/wi:220"
+            },
+            new Kitap
+            {
+                Ad = "Çile",
+                Yazar = "Necip Fazýl Kýsakürek",
+                SayfaSayisi = 502,
+                Tur = "Þiir",
+                BasimYili = 1962,
+                ResimYolu = "https://img.kitapyurdu.com/v1/getImage/fn:11303273/wh:true/wi:220"
+            },
+            new Kitap
+            {
+                Ad = "Kürk Mantolu Madonna",
+                Yazar = "Sabahattin Ali",
+                SayfaSayisi = 160,
+                Tur = "Roman",
+                BasimYili = 1943,
+                ResimYolu = "https://img.kitapyurdu.com/v1/getImage/fn:11340150/wh:true/wi:220"
+            }
+        );
+        context.SaveChanges();
+    }
 }
 
 app.Run();
